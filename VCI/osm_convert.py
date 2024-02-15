75c46de686a5609bba9962c42b8f03987db84a82
henriquecscode <henriquecostasousa.hcs@gmail.com>
2024-02-15 12:34:15 +0000

    dynamically use osm files for initial netconvert

--- VCI/osm_convert.py
+++ VCI/osm_convert.py
@@ -7,7 +7,7 @@ from utils.datetime_utils import get_current_time_file_format
 OSM_FOLDER = "osm"
 NETWORKS_DIR = "networks"
 
-def main(config_file: str, options: dict, *args, **kwargs):
+def main(osm_folder: str, config_file: str, options: dict, *args, **kwargs):
     file_datetime = get_current_time_file_format()
 
     config_file_dir = os.path.dirname(config_file)
@@ -16,26 +16,30 @@ def main(config_file: str, options: dict, *args, **kwargs):
 
     copy_config_file_basename = f"{config_file_name}_{file_datetime}{config_file_ext}"
     copy_config_file = os.path.join(config_file_dir, copy_config_file_basename)
-    osm_copy_config_file = os.path.relpath(copy_config_file, OSM_FOLDER)
+    osm_copy_config_file = os.path.relpath(copy_config_file, osm_folder)
 
+    # Get osm files in osm_folder
+    osm_file_list = [f for f in os.listdir(osm_folder) if f.endswith(".osm")]
+    osm_files = ",".join(osm_file_list)
 
     osm_config_file = config_file_basename
-    shutil.copy(config_file, os.path.join(OSM_FOLDER, config_file_basename))
+    shutil.copy(config_file, os.path.join(osm_folder, config_file_basename))
 
     if "output_file" in options:
         output_file = options["output_file"]
     else:
         output_file= os.path.join(NETWORKS_DIR, f"{config_file_name}_{file_datetime}.net.xml")
-    osm_output_file = os.path.relpath(output_file, OSM_FOLDER)
+    osm_output_file = os.path.relpath(output_file, osm_folder)
 
-    os.chdir(OSM_FOLDER)
+    os.chdir(osm_folder)
 
     # Run command with save configuration (doesn't execute, just saves)
-    command = f"netconvert --save-configuration {osm_copy_config_file} --configuration-file {osm_config_file} --output-file {osm_output_file}"
+    base_command = f"netconvert --osm-files {osm_files} --configuration-file {osm_config_file} --output-file {osm_output_file}"
+    command = f"{base_command} --save-configuration {osm_copy_config_file}"
     run_system(command)
 
     # Actually run netconvert
-    command = f"netconvert --configuration-file {osm_config_file} --output-file {osm_output_file}"
+    command = base_command
     run_system(command)
 
     os.remove(config_file_basename)
@@ -47,6 +51,7 @@ def parse_arguments():
     parser = argparse.ArgumentParser(description='Description of your script')
 
     # Add command line arguments
+    parser.add_argument("--osm-folder", "-f", type=str, help="Path to the folder containing the OSM file")
     parser.add_argument("--output-file", "-o", type=str, help="Path to the output file")
     parser.add_argument("--config_file", type=str, help="Path to the configuration file")
     #parser.add_argument('arg1', type=str, help='Description of arg1')
@@ -63,12 +68,17 @@ if __name__ == "__main__":
     if args.output_file is not None:
         options["output_file"] = args.output_file
 
+    if args.osm_folder is None:
+        osm_folder = OSM_FOLDER
+    else:
+        osm_folder = args.osm_folder
+
     if args.config_file is None:
-        config_file = "networks/base.cfg"
+        config_file = "netconvert_configs/base.cfg"
     else:
         config_file = args.config_file
 
 
 
     # Call the main function with parsed arguments
-    main(config_file, options)
+    main(osm_folder, config_file, options)
