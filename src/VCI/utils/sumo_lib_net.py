fda33aa5c87e88bc6d0e961760adb65187b7964f
henriquecscode <henriquecostasousa.hcs@gmail.com>
2024-02-15 17:09:28 +0000

    util lib to lad the sumo net

---
+++ src/VCI/utils/sumo_lib_net.py
@@ -0,0 +1,8 @@
+import sumolib
+import os
+
+def get_net_file(sumo_net_file: str) -> sumolib.net.Net:
+    if not os.path.exists(sumo_net_file):
+        raise FileNotFoundError(f"File {sumo_net_file} does not exist")
+    net = sumolib.net.readNet(sumo_net_file)
+    return net
